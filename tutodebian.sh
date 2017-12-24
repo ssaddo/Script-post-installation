08f0bda8350f7e76185ddda0ffdf7919bd418349
tutodebian <31676566+stephaneeee@users.noreply.github.com>
2017-12-24 02:56:46 +0100

    Update tutodebian.sh

--- tutodebian.sh
+++ tutodebian.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/sh
+#!/bin/sh
 
 #######################################
 # Script pour automatiser la post-installation de machine sous Debian 9 (Stretch).
@@ -8,7 +8,7 @@
 # Modification des dépôts
 echo -e "\e[38;5;75mModification des dépôts..\e[0m"
 rm /etc/apt/sources.list
-wget -P /etc/apt/ https://tutodebian.fr/perso/debian/sources.list
+wget -P /etc/apt/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/sources.list
 
 # Mise à jour du système
 echo -e "\e[38;5;75mMise à jour du système..\e[0m"
@@ -24,25 +24,25 @@ sed -i "21i\ "$nom" ALL=(ALL:ALL) ALL" /etc/sudoers
 # Installation du thème graphique
 echo -e "\e[38;5;75mInstallation du thème graphique..\e[0m"
 apt-get install autoconf automake libgtk-3-dev git gnome-themes-standard gtk2-engines-murrine arc-theme conky-all lightdm-gtk-greeter-settings -y
-wget -P /usr/share/backgrounds https://tutodebian.fr/perso/debian/background.png
-wget -P /usr/share/backgrounds https://tutodebian.fr/perso/debian/wallpaperflat.png
-wget -P /usr/share/icons https://tutodebian.fr/perso/debian/logodebianflat.png
-wget -P /usr/share/icons https://tutodebian.fr/perso/debian/logomenu.png
+wget -P /usr/share/backgrounds https://raw.githubusercontent.com/stephaneeee/tutodebian/master/background.png
+wget -P /usr/share/backgrounds https://raw.githubusercontent.com/stephaneeee/tutodebian/master/wallpaperflat.png
+wget -P /usr/share/icons https://raw.githubusercontent.com/stephaneeee/tutodebian/master/logodebianflat.png
+wget -P /usr/share/icons https://raw.githubusercontent.com/stephaneeee/tutodebian/master/logomenu.png
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install-papirus-root.sh | sh
 rm /home/$nom/.config/xfce4/terminal/terminalrc
-wget -P /home/$nom/.config/xfce4/terminal/ https://tutodebian.fr/perso/debian/terminalrc
+wget -P /home/$nom/.config/xfce4/terminal/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/terminalrc
 chown $nom:$nom /home/$nom/.config/xfce4/terminal/terminalrc
 rm /home/$nom/.config/xfce4/panel/whiskermenu-7.rc
-wget -P /home/$nom/.config/xfce4/panel/ https://tutodebian.fr/perso/debian/whiskermenu-7.rc
+wget -P /home/$nom/.config/xfce4/panel/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/whiskermenu-7.rc
 chown $nom:$nom /home/$nom/.config/xfce4/panel/whiskermenu-7.rc
 rm /home/$nom/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-wget -P /home/$nom/.config/xfce4/xfconf/xfce-perchannel-xml/ https://tutodebian.fr/perso/debian/xfce4-panel.xml
+wget -P /home/$nom/.config/xfce4/xfconf/xfce-perchannel-xml/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/xfce4-panel.xml
 chown $nom:$nom /home/$nom/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 
 # Configuration de grub et lightdm
 echo -e "\e[38;5;75mConfiguration de grub et lightdm..\e[0m"
 rm /etc/lightdm/lightdm-gtk-greeter.conf
-wget -P /etc/lightdm/ https://tutodebian.fr/perso/debian/lightdm-gtk-greeter.conf
+wget -P /etc/lightdm/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/lightdm-gtk-greeter.conf
 sed -i "105i\if background_image /usr/share/backgrounds/wallpaperflat.png; then" /boot/grub/grub.cfg
 sed -i "107i\if background_image /usr/share/backgrounds/wallpaperflat.png; then" /boot/grub/grub.cfg
 
@@ -53,16 +53,13 @@ apt-get install thunderbird-l10n-fr filezilla enigmail lightning-l10n-fr gtk-red
 # Modification du bash
 echo -e "\e[38;5;75mModification du bash..\e[0m"
 rm /home/$nom/.bashrc
-wget -P /home/$nom/ https://tutodebian.fr/perso/debian/.bashrc
+wget -P /home/$nom/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/.bashrc
 cp /home/$nom/.bashrc /root/.bashrc
-rm /usr/bin/screenfetch
-wget -P /usr/bin/ https://tutodebian.fr/perso/debian/screenfetch
-sudo chmod 755 /usr/bin/screenfetch
 
 # configuration de conky
 echo -e "\e[38;5;75mconfiguration de conky..\e[0m"
-wget -P /home/$nom/ https://tutodebian.fr/perso/debian/.conkyrc
-wget -P /home/ https://tutodebian.fr/perso/debian/.debupdates.sh
+wget -P /home/$nom/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/.conkyrc
+wget -P /home/ https://raw.githubusercontent.com/stephaneeee/tutodebian/master/.debupdates.sh
 chmod +x /home/.debupdates.sh
 
 # Configuration de Firefox
